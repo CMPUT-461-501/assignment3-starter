7c22a5ae3bf33d99f1cdc4b5f7a3eb1ffc1e1e41
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-10-08 15:21:26 -0600

    Initial commit

---
+++ code/langid.py
@@ -0,0 +1,3 @@
+# This program should loop over the files listed in the input file directory,
+# assign perplexity with each language model,
+# and produce the output as described in the assignment description.
